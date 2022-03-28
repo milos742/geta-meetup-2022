dcf0682bdc5cd686810dff571125afcb8f0ba6d8
vuk.mistovic <vuk.mistovic@getadigital.com>
2022-03-28 19:52:01 +0200

    Remove Console log

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,13 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 import { useGameContext } from "../GameProvider/GameProvider";
 import { Row } from "../Row/Row";
 import style from "./_header.module.css";
 
 export function Header() {
 	const gameContext = useGameContext();
-
-	const [classes, setClasses] = useState();
 	
 	const rounds = gameContext.historyHits.filter((p) => {
 		return p.playerId === gameContext.historyHits[0].playerId;
-	});
-
-	useEffect(() => {
-		// This gets called after every render, by default
-		// (the first one, and every one after that)
-		console.log("render!");
-
-		// If you want to implement componentWillUnmount,
-		// return a function from here, and React will call
-		// it prior to unmounting.
-		return () => console.log("unmounting...");
 	});
 
 	return (
